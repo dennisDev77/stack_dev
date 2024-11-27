78542d06554dab8f604850502055735ceec32fbf
dennisDev77 <diniscamela5@gmail.com>
2024-11-27 18:29:16 +0100

    --amend

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer=()=>{
          <section className={` ${styles.section} text-personal-gray flex flex-col flex-wrap items-center justify-center gap-2`} >
 
             <div className='text-center font-light text-personal-gray pb-3'>
-            <p className='text-personal-gray '>Desenvolvido por <a href='https://github.com/dennisDev77'>Dinis Camela</a></p>
+            <p className='text-personal-gray '>Desenvolvido por <a href='https://diniscamela.vercel.app'>Dinis Camela</a></p>
             </div>
          </section>
         </>
